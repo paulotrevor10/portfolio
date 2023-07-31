c1702a759cbc01fab50f7979de4763d316b0e952
Lorgie <paulotrevor.regacho.d@bulsu.edu.ph>
2023-07-31 22:56:50 +0800

    fix again

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,57 +7,63 @@ import { MatDialog } from '@angular/material/dialog';
 import { SkillModalComponent } from '../skill-modal/skill-modal.component';
 import { Howl } from 'howler';
 
-
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
-  formData: any = {};
-  
+  fullName: string = '';
+  email: string = '';
+  message: string = '';
+
   soundThankYou = new Howl({
     src: ['assets/thank-you.mp3'],
   });
-  
+
   soundError = new Howl({
     src: ['assets/error.mp3'],
   });
-  
-  
+
   constructor(private http: HttpClient, public dialog: MatDialog) {}
 
   submitForm() {
     const formURL = 'https://formspree.io/f/xgejwplg'; // Replace with your Formspree form ID
-    this.http.post(formURL, this.formData)
-    .pipe(
-      catchError((error: HttpErrorResponse) => {
-      
-        this.openModal('email-dialog-false');
-        this.soundError.play();
-        return throwError('Something went wrong. Please try again later.');
-      })
-    )
-    .subscribe((response) => {
-    
-      this.formData = {
-        email: '',
-        message: ''
-      };
-      this.openModal('email-dialog-true');
-      this.soundThankYou.play();
-    });
+
+    const formData = {
+      fullName: this.fullName,
+      email: this.email,
+      message: this.message,
+    };
+
+    if (!this.fullName || !this.email || !this.message) {
+      this.openModal('email-dialog-false');
+      this.soundError.play();
+    }
+    else {
+      this.http
+        .post(formURL, formData)
+        .pipe(
+          catchError((error: HttpErrorResponse) => {
+            this.openModal('email-dialog-false');
+            this.soundError.play();
+            return throwError('Something went wrong. Please try again later.');
+          })
+        )
+        .subscribe((response) => {
+          this.openModal('email-dialog-true');
+          this.soundThankYou.play();
+        });
+    }
   }
-  
+
   openModal(id: string): void {
     const dialogRef = this.dialog.open(SkillModalComponent, {
-      data: { id: id }
+      data: { id: id },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log('The dialog was closed');
     });
   }
-  
-  
 }
